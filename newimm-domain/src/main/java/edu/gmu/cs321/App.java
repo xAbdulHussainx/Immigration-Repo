529c22f9ef70c2522741531bb4179f4e9114f327
Abdul Hussain <abdulhussain675@gmail.com>
2024-10-09 19:04:29 -0400

    anything

---
+++ newimm-domain/src/main/java/edu/gmu/cs321/App.java
@@ -0,0 +1,13 @@
+package edu.gmu.cs321;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
